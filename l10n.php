837172016507c1e73270506eb7b48e2182b113c0
Anton Korotkov <antonkorotkoff@gmail.com>
2014-09-18 19:06:48 +0300

    missing files

---
+++ l10n.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Localization.
+ * Fixes localization for some dynamic values.
+ * Can be used for javascript localization
+ *
+ * @since 0.36.0
+ * @author peshkov@UD
+ * @package WP-CRM
+ */
+
+$l10n = array(
+
+  // Meta boxes Titles
+  'actions' => __( 'Actions', 'wp_crm' ),
+  'user_activity_history' => __( 'User Activity History', 'wp_crm' ),
+  'primary_information' => __( 'Primary Information', 'wp_crm' ),
+  'special_actions' => __( 'Special Actions', 'wp_crm' ),
+
+);
+
