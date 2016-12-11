1ca21ed7efac9639208059bc65ab393bcf3a0989
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2016-12-12 05:58:48 +0600

    Issue #79

--- lib/class_notification.php
+++ lib/class_notification.php
@@ -41,7 +41,8 @@ class WP_CRM_N {
 
     $wpp_agents_id = get_post_meta($associated_object->ID, 'wpp_agents', true);
     $agent_data = get_userdata( $wpp_agents_id );
-    if (!empty($associated_object) && $wpp_agents_id && property_exists($agent_data, 'user_email')) {
+
+    if ($wpp_agents_id && !empty($agent_data->user_email)) {
       $replace_with['agent_email'] = $agent_data->user_email;
     }
 
