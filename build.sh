06700c483216253160f800572315e79943a9c53d
Anton <antonkorotkoff@gmail.com>
2019-05-01 11:45:19 +0300

    fix to release builder script

--- build.sh
+++ build.sh
@@ -195,6 +195,7 @@ else
   rm -rf makefile
   rm -rf package.json
   rm -rf test
+  rm -rf .gitignore
   echo "Be sure we do not add .git directories"
   find ./vendor -name .git -exec rm -rf '{}' \;
   echo "Be sure we do not add .svn directories"
